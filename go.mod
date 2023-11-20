0fa312269f7a94df7e7d361f71772130e0c6a5ca
Mokhtar Bacha <mokhtar@joinformal.com>
2023-11-20 14:02:32 -0800

    add debug logs to the lambda function

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.21
 require (
 	github.com/aws/aws-lambda-go v1.41.0
 	github.com/aws/aws-sdk-go v1.45.17
+	github.com/rs/zerolog v1.31.0
 )
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log/slog"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rs/zerolog/log"
 )
 
 type Response struct {
@@ -21,30 +21,38 @@ var (
 )
 
 func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Debug().Msgf("Received event: %s", event.Body)
+
 	parsed, err := parseFormalEncryptedData(event.Body)
 	if err != nil {
-		slog.Error("Error happen in parseFormalEncryptedData", err)
+		log.Error().Err(err).Msg("Error happen in parseFormalEncryptedData")
 		return events.APIGatewayProxyResponse{StatusCode: 400, Body: err.Error(), Headers: headers}, nil
 	}
 
+	log.Debug().Msgf("Parsed body: %+v", parsed)
+
 	dataKey, err := decryptDataKey(parsed.KmsKeyRegion, parsed.KmsKeyId, []byte(parsed.EncryptedKey))
 	if err != nil {
-		slog.Error("Error happen in decryptDataKey", err)
+		log.Error().Err(err).Msg("Error happen in decryptDataKey")
 		return events.APIGatewayProxyResponse{StatusCode: 500, Body: err.Error(), Headers: headers}, nil
 	}
 
 	decrypted, err := decryptString(parsed.EncryptedData, dataKey)
 	if err != nil {
-		slog.Error("Error happen in decryptString", err)
+		log.Error().Err(err).Msg("Error happen in decryptString")
 		return events.APIGatewayProxyResponse{StatusCode: 500, Body: err.Error(), Headers: headers}, nil
 	}
 
-	response := Response{Message: string(decrypted)}
+	log.Debug().Msgf("Decrypted data: %s", decrypted)
+
+	response := Response{Message: decrypted}
 	responseBody, err := json.Marshal(&response)
 	if err != nil {
-		slog.Error("Error happen in json.Marshal", err)
+		log.Error().Err(err).Msg("Error happen in json.Marshal")
 		return events.APIGatewayProxyResponse{StatusCode: 500, Body: "Error marshalling the response body", Headers: headers}, nil
 	}
+
+	log.Debug().Msgf("Response body: %s", responseBody)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
